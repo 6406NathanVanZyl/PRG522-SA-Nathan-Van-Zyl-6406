80eb6b529dca93b54191657174d3bb889a2084a2
Nathan Van Zyl <6406@ctucareer.co.za>
2021-09-15 19:53:18 +0200

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRG522_SA_Nathan_Van_Zyl_6406.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRG522_SA_Nathan_Van_Zyl_6406.Startup))]
+namespace PRG522_SA_Nathan_Van_Zyl_6406
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
